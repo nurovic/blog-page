ef24ec7026bbd02e0c44d1fdb6feca47185f488c
nurovic <mertnurovic@gmail.com>
2022-07-06 23:56:55 +0300

    Fixed text

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const ProjectsList = () => {
   return (
     <div>
-        <div className='bg-gray-400 h-40 mx-40 flex flex-row'>
-            <img className='h-full w-40' src={require("../img/1654120704258.jpg")} alt="" />
-            <div className='ml-6 mt-2'>
+        <div className='border-2 h-40 mx-40 flex flex-row rounded-3xl'>
+            <img className='rounded-l-3xl h-full w-40 object-cover' src={require("../img/vue.jpeg")} alt="" />
+            <div className='ml-6 mt-2 '>
                 <a href="https://main--meek-speculoos-91d879.netlify.app/">Onlince Course</a>
-                <h3>JavaScript, Node JS, Vue JS, Mongoose, MongoDB, Stripe JS, </h3>
+                <h3>JavaScript, Node JS, Vue JS, Mongoose, MongoDB, Stripe JS, Cloudiinary, Node Mailler</h3>
                 <h3>Description</h3>
             </div>
         </div>
